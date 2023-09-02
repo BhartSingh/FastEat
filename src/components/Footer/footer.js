5fb257fc56a900a6e16945a98d64118ce9956536
BhartSingh <monuranoliya731@gmail.com>
2023-09-02 18:11:43 +0530

    Initial commit

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Footer/footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <section className="copyright ">
+        <p className="reserve">
+          <span>©</span> 2023 Rights Reserved,
+          <span className="name">Bhart Singh</span>
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default Footer;
